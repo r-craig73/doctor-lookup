5a7edb5583e5c7138b2c0f3c156000bf23748612
Ron Craig <ron.craig@comcast.net>
2018-03-23 22:32:16 -0700

    pass 5 specs using on js file, edit readme

--- readme.md
+++ readme.md
@@ -6,25 +6,18 @@
 ### GitHub repository: https://github.com/r-craig73/doctor-lookup
 
 ## Description
-### An app to find a doctor in Portland, OR (using BetterDoctor API) based on either a medical condition or doctor's name.
+### An app to list  doctors (up to 90 doctors) in Portland, OR (using BetterDoctor API), based on either a medical condition or doctor's name.
 
 ## Business Specifications
 
 | Spec   | Input   | Output  | Status   |
 |--------|:-------:|---------|----------|
 | User inputs a medical issue and receive a list of doctors. | "rash" | "Susan Koch" | Ongoing |
-| User inputs a name (First or Last Name) to receive a list of doctors. | "Lowe" | "Joe Lowe" | Ongoing |
-| If results from Spec 1 or Spec 2 includes any doctors, the doctor's information should be included. | "rash" | "Susan Koch" | Ongoing |
-| The app should return an error notification if the API call results in an error (any message not a 200 OK) | condition: "allergy" | "Ahhhh choo! An error occurred, please try again." | Ongoing |
-| If there are no doctors available after the query response, the app returns a message stating no doctors meet the criteria (not route error). | name: "Seuss" OR condition: "cowbell" | "There are no doctors matching your search." OR "There are no doctors available to help with your medical issue." | Ongoing |
+| User inputs a name (First, Middle or Last name) to receive a list of doctors. | "Lowe" | "Joe Lowe, MD" | Completed |
+| If results from Spec 1 or Spec 2 includes any doctors, the doctor's information should be included. | "rash" | "Susan Koch, MD", "123 Main Street", 360-867-5309, www.susank_skin_clinic.com, Accepting patients: Yes | Completed |
+| The app should return an error notification if the API call results in an error (any message not a 200 OK) | condition: "allergy" | "Ahhhh choo! An error occurred, please try again." | Completed |
+| If there are no doctors available after the query response, the app returns a message stating no doctors meet the criteria (not route error). | name: "Seuss" OR condition: "cowbell" | "There are no doctors matching your search." OR "There are no doctors available to help with your medical issue." | Completed |
 
-## Technologies Used
-
-```
-Application: JavaScript, jQuery, Bootstrap, HTML
-Testing: Jasmine, Karma
-Environment: npm, webpack, babel, esLint
-```
 ## Installation setup
 1. Install webpack dependencies using Roger Yee's npm-webpacker script (https://github.com/ryee926/npm-webpacker)
 * npm plugins
@@ -62,8 +55,30 @@ Environment: npm, webpack, babel, esLint
 * Babel
 - [x] babel loader
 
-## Known Bugs
-### Still need to complete the Planning section (see below).
+2. Install npm packages and dependencies.
+```
+$ npm install
+$ npm run build
+```
+
+3. Start webserver to run app.
+```
+$ npm run start
+```
+
+## Technologies Used
+
+```
+Application: JavaScript, jQuery, Bootstrap, HTML
+Testing: Jasmine, Karma
+Environment: npm, webpack, babel, esLint
+```
+
+## Known Bugs and Issues
+### Still need to complete the Planning section below.
+### Need to split js main files into two files.
+1. Receive form input (main.js).
+2. Make API request and display results and warnings (doctor-lookup.js).
 
 ### MIT License
 
@@ -71,26 +86,19 @@ Copyright (c) 2018 **_RC productions_**
 
 ##### Planning
 
-1. Finish Business Specs
-- [ ] Spec 1: User inputs a medical issue and receive a list of doctors in the Portland, OR area, input ("rash", "flu" or "sore throat"), output (Dr. Bob Samuel, Dr. Suzzane Lowe, etc.).
-- [ ] Spec 2: User inputs a name to receive a list of doctors in the Portland, OR area, input ("Lowe"), output (Dr. Suzzane Lowe, Dr. Lowe Bowen, etc.).
-- [ ] Spec 3: If results from Spec 1 or Spec 2 includes any doctors, the doctor's information should be included (first name, last name, address, phone number, website and whether or not doctor is accepting new patients). input ("rash"), output (Dr. Bob Samuel, 123 Main Street, 360-867-5309, www.drbobs.com, accepting patients).
-- [ ] Spec 4: The app should return an error notification if the API call results in an error (any message not a 200 OK). input ("rash"), output (Ahhhh choo! An error occurred, please try again, Message 4xx).
-- [ ] Spec 5: If there are no doctors available after the query response, the app returns a message stating no doctors meet the criteria (not route error). input ("Dr. Seuss" or "cowbell fever"), output ("There are no doctors matching that name" or "There are no doctors available to help with your medical issue").
-
-2. Integration
+1. Integration
 - [x] Read through BetterDoctor API to generate medical issue and doctor's name queries.
 - [x] Verify API_KEY and queries are working through postman.
 - [x] Generate html template page with two form options, medical condition and doctor's name.
-- [ ] Use main js file to combine two searches.
-- [ ] Verify query results are working and viewed in HTML.
+- [x] Use main js file to combine two searches.
+- [x] Verify query results are working and displayed in HTML.
 - [ ] Separate js business and user interface between main and doctor-lookup.  Create two classes showDoctorByCondition and showDoctorsByName.
 
-3. UX/UI
-- [ ] Include and modify bootstrap.
+2. UX/UI
+- [x] Include and modify bootstrap.
 - [ ] Develop a custom style, doctor themed.
 
-4. Polish
+3. Polish
 - [ ] Cleanup README file.
 - [ ] Delete unused files.
 - [ ] Refactor js and html.
--- src/main.js
+++ src/main.js
@@ -5,25 +5,66 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('.start-over').hide();
+  $('.showDoctorsByCondition').hide();
+  $('.showDoctorsByName').hide();
 
   $('.condition-button').click(function() {
     event.preventDefault();
     $('.start-over').show();
     $('.doctor-name').hide();
     $('showDoctorsByCondition').empty();
+    $('showDoctorsByCondition').show();
     let condition = $('#condition').val();
-    console.log(condition);
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.523%2C-122.676%2C20&sort=distance-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`)
-      .then(function(response) {
-        console.log(response);
-        let acceptingPatients = response.data[0].practices[0].accepts_new_patients;
-        console.log(acceptingPatients);
-        let firstName = response.data[0].profile.first_name;
-        console.log(firstName);
-        let lastName = response.data[0].profile.last_name;
-        console.log(lastName);
-        let street = response.data[0].practices[0].visit_address.street;
-        console.log(street);
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.523%2C-122.676%2C20&sort=distance-asc&skip=0&limit=90&user_key=${process.env.exports.apiKey}`)
+    .then(function(response) {
+      let total = response.meta.total;
+      $('.showDoctorsByCondition').show();
+      $('.showDoctorsByCondition').append(`Here is a list of doctor(s) in the Portland, OR area matching your medical condition:` + '<br>' + '<br>');
+      let acceptingPatients = [];
+      let firstName = [];
+      let middleName = [];
+      let lastName = [];
+      let title = [];
+      let street = [];
+      let city = [];
+      let state = [];
+      let zipCode = [];
+      let phoneNumber = [];
+      let urlSite = [];
+      if (total == 0) {
+        $('.showDoctorsByCondition').text(`There are no doctors matching your search.`);
+        } else {
+        for(let i = 0; i < response.meta.total ; i++) {
+          acceptingPatients[i] = response.data[i].practices[0].accepts_new_patients;
+          firstName[i] = response.data[i].profile.first_name;
+          middleName[i] = response.data[i].profile.middle_name;
+          lastName[i] = response.data[i].profile.last_name;
+          title[i] = response.data[i].profile.title;
+          street[i] = response.data[i].practices[0].visit_address.street;
+          city[i] = response.data[i].practices[0].visit_address.city;
+          state[i] = response.data[i].practices[0].visit_address.state;
+          zipCode[i] = response.data[i].practices[0].visit_address.zip;
+          phoneNumber[i] = response.data[i].practices[0].phones[0].number;
+          urlSite[i] = response.data[i].practices[0].website;
+          if (typeof urlSite[i] == "undefined") {
+            urlSite[i] = "Not Available";
+          }
+          if (typeof middleName[i] == "undefined") {
+            middleName[i] = "";
+          }
+          if (acceptingPatients[i] == true) {
+            acceptingPatients[i] = "Yes";
+          } else {
+            acceptingPatients[i] = "No";
+          }
+          if (typeof title[i] == "undefined") {
+            title[i] = " ";
+          } else {
+            title[i] = ", " + title[i];
+          }
+          $('.showDoctorsByCondition').append(firstName[i] + " " + middleName[i] + " " + lastName[i] + title[i] + '<br>' + "  " + street[i] + '<br>' + city[i] + ", " + state[i] + " " + zipCode[i] + '<br>' + "Phone number: " + phoneNumber[i] + '<br>' + "Accepting patients? " + acceptingPatients[i] + '<br>' + "Website: " + urlSite[i] + '<br>' + '<br>');
+        }
+      }
       }).fail(function(error) {
         $('.showErrors').text(`Ahhhh chooo! There was an error processing your request: Please try again.`);
       });
@@ -34,13 +75,58 @@ $(document).ready(function() {
     $('.start-over').show();
     $('.condition').hide();
     $('showDoctorsByName').empty();
+    $('showDoctorsByName').show();
     let name = $('#doctor-name').val();
-    console.log(name);
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.523%2C-122.676%2C20&sort=distance-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.523%2C-122.676%2C20&sort=distance-asc&skip=0&limit=90&user_key=${process.env.exports.apiKey}`)
       .then(function(response) {
-        console.log(response);
-        let acceptingPatients = response.data[0].practices[0].accepts_new_patients;
-        console.log(acceptingPatients);
+        let total = response.meta.total;
+        $('.showDoctorsByName').show();
+        $('.showDoctorsByName').append(`Here is a list of doctor(s) in the Portland, OR area matching your search:` + '<br>' + '<br>');
+        let acceptingPatients = [];
+        let firstName = [];
+        let middleName = [];
+        let lastName = [];
+        let title = [];
+        let street = [];
+        let city = [];
+        let state = [];
+        let zipCode = [];
+        let phoneNumber = [];
+        let urlSite = [];
+        if (total == 0) {
+          $('.showDoctorsByName').text(`There are no doctors matching your search.`);
+          } else {
+          for(let i = 0; i < response.meta.total ; i++) {
+            acceptingPatients[i] = response.data[i].practices[0].accepts_new_patients;
+            firstName[i] = response.data[i].profile.first_name;
+            middleName[i] = response.data[i].profile.middle_name;
+            lastName[i] = response.data[i].profile.last_name;
+            title[i] = response.data[i].profile.title;
+            street[i] = response.data[i].practices[0].visit_address.street;
+            city[i] = response.data[i].practices[0].visit_address.city;
+            state[i] = response.data[i].practices[0].visit_address.state;
+            zipCode[i] = response.data[i].practices[0].visit_address.zip;
+            phoneNumber[i] = response.data[i].practices[0].phones[0].number;
+            urlSite[i] = response.data[i].practices[0].website;
+            if (typeof urlSite[i] == "undefined") {
+              urlSite[i] = "Not Available";
+            }
+            if (typeof middleName[i] == "undefined") {
+              urlSite[i] = "";
+            }
+            if (acceptingPatients[i] == true) {
+              acceptingPatients[i] = "Yes";
+            } else {
+              acceptingPatients[i] = "No";
+            }
+            if (typeof title[i] == "undefined") {
+              title[i] = " ";
+            } else {
+              title[i] = ", " + title[i];
+            }
+            $('.showDoctorsByName').append(firstName[i] + " " + middleName[i] + " " + lastName[i] + title[i] + '<br>' + "  " + street[i] + '<br>' + city[i] + ", " + state[i] + " " + zipCode[i] + '<br>' + "Phone number: " + phoneNumber[i] + '<br>' + "Accepting patients? " + acceptingPatients[i] + '<br>' + "Website: " + urlSite[i] + '<br>' + '<br>');
+          }
+        }
       }).fail(function(error) {
         $('.showErrors').text(`Ahhhh chooo! There was an error processing your request: Please try again.`);
       });
@@ -50,6 +136,5 @@ $(document).ready(function() {
     event.preventDefault();
     location.reload();
   });
-
 
 });
